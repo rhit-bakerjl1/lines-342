d4a6cc704a598186223b07cffb4a8d81c9fb676d
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-05-20 06:03:31 -0400

    Final changes, added transportation analysis

--- PB_2.m
+++ PB_2.m
@@ -1,28 +1,9 @@
-close;
-clear;
-clc;
-
-% Declaring Variables
-y   = [15.6; 21.7; 3.1; 14.7; 6.9; 16.1; 7.5; 16.1; 20.2; 13.3; 15.0; 23.3];
-x1  = [2.0; 3.6; 0.9; 2.4; 0.1; 2.6; 0.7; 1.0; 2.4; 1.9; 2.3; 3.9];
-x2  = [4.7; 5.7; 1.6; 4.8; 4.4; 6.0; 2.6; 5.7; 4.5; 5.0; 4.8; 6.9];
-x3  = [2.9; 4.9; 1.4; 3.3; 1.6; 3.7; 1.6; 3.0; 3.1; 3.4; 3.3; 4.8];
-
-% Solve for Values
-% A   = [ones(size(y)), x1, x2, x3];
-% [alpha, ~, ~, ~, stats]     = regress(y,A);
-A   = [x1, x2, x3];
-mdl     = fitlm(A, y);
-A_new   = [x1, x2];
-mdl2    = fitlm(A_new, y);
-[coeffs, A_found, R_sq] = func_fwd_regression(A, y);
-
-%% Project 1 Time!
+%% Problem 2 Time!
 % options
 make    = 1;
 cyl_cat = 1;
-data1   = readcell('car_data.xls');
-data    = readmatrix('car_data.xls');
+data1   = readcell('Monthly_Transportation_Statistics_modified.xlsx');
+data    = readmatrix('Monthly_Transportation_Statistics_modified.xlsx');
 % Make: Buick, Cadillac, Chevrolet, Pontiac, SAAB, Saturn
 carMake = data1(2:end,3);
 x3p     = strcmp(carMake, 'Cadillac');
@@ -34,8 +15,8 @@ makeMat = [x3p, x3pp, x3ppp, x3pppp, x3ppppp];
 % Cylinders: 4, 6, 8
 x7p     = data(:,7) == 6;
 x7pp    = data(:,7) == 8;
-y_car   = data(:,1);
-A_car   = [data(:,[2,8:12])];
+y_car   = data(:,5);
+A_car   = [data(:,[10:14,20:85,87:107,110:123,125:128,130,132,134,135,137])];
 if (cyl_cat)
     A_car = [A_car, x7p, x7pp];
 else
@@ -51,10 +32,10 @@ y_mod   = [ones(size(y_car)) A_car]*coeffs';
 
 figure(1);
 clf;
-plot(y_car, '-');
+plot(y_car, 'o');
 hold on;
-plot(y_mod);
-ylabel("Car Price");
+plot(y_mod, '-');
+ylabel("Highway Fatalities");
 legend("Data", "Modeled");
 
 figure(2);
